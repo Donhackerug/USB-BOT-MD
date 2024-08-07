bfeb5039c9d7b6ef6b5c92af7b043e8749e80aa3
Donhackerug <donhackerug@gmail.com>
2024-08-07 07:58:01 +0300

    Add files via upload

---
+++ Gallery/Iib/weeb.js
@@ -0,0 +1,24 @@
+const prefix = global.prefa
+  const pushname = m.pushName || "No Name"
+
+const weebmenu=`
+「 *Maria BOT* 」
+♡ۣۜۜ፝͜͜͡͡✿➤ *INFO BOT*
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Name: USB BOT MD
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Version: 1.0
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Type: VPS
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Owner: 𝐃𝐎𝐍 𝐓𝐄𝐂𝐇 𝐈𝐍𝐂
+  ♡ۣۜۜ፝͜͜͡͡✿➤ wa.me/256742366969
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Prefix: 「  ${prefix}  」
+ ♡ۣۜۜ፝͜͜͡͡✿➤Menu Type: 𝑾𝒆𝒆𝒃-𝒎𝒆𝒏𝒖
+ ----------------------------------
+🍁 *Weeb Commands* 🍁
+⌬${prefix}crossplay
+⌬${prefix}neko
+⌬${prefix}loli
+⌬${prefix}wallpaper
+⌬${prefix}waifu
+
+ `
+ module.exports = weebmenu
+ 
